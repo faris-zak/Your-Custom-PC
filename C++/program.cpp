0538ab49340a902460f6c9507a677c62e9a340ac
Al-Faris Mujahid AlZakwani <107947850+faris-zak@users.noreply.github.com>
2024-06-19 03:31:47 +0400

    Add files via upload

---
+++ C++/program.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int n;
+    double budget, total = 0;
+    string 
+    cout << "Enter your budget(OMR): "; cin >> budget;
+    cout << "\n1 - Graphics Cards" << "\n2 - CPU" << "\n3 - Monitors"<< "\n4 - RAM";
+    cout << "\nchoice one of them(num): " ; cin >> n;
+
+    switch (n){
+    case 1:
+        
+        break;
+
+    case 2:
+        
+        break;
+
+    case 3:
+        
+        break;
+
+    case 4:
+        
+        break;
+    
+    default:
+        cout << "Invalid choice";
+        break;
+    }
+
+    return 0;
+}
