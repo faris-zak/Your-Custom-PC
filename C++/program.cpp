024720aa3cb528b8463843f9a249b7a07fde23b5
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-06-19 09:56:33 +0400

    commit

--- C++/program.cpp
+++ C++/program.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 int main(){
     int n;
     double budget, total = 0;
-    string 
+    string cpu, graphicsCard, monitor, ram;
     cout << "Enter your budget(OMR): "; cin >> budget;
-    cout << "\n1 - Graphics Cards" << "\n2 - CPU" << "\n3 - Monitors"<< "\n4 - RAM";
-    cout << "\nchoice one of them(num): " ; cin >> n;
+    cout << "-----";
 
-    switch (n){
-    case 1:
-         
-        break;
+    for(int i = 0; i < 4; i++){
+        cout << "\n1 - Graphics Cards" << "\n2 - CPU" << "\n3 - Monitors" << "\n4 - RAM" << "\n5 - Exit";
+        cout << "\nEnter your choice(num): "; cin >> n;
 
-    case 2:
-        
-        break;
+        switch (n){
+            case 1:
+                cout << "Enter Graphics Card name: "; cin >> graphicsCard;
+                break;
 
-    case 3:
-        
-        break;
+            case 2:
+                cout << "Enter CPU name: "; cin >> cpu;
+                break;
 
-    case 4:
-        
-        break;
-    
-    default:
-        cout << "Invalid choice";
-        break;
-    }
+            case 3:
+                cout << "Enter Monitor name: "; cin >> monitor;
+                break;
 
+            case 4:
+                cout << "Enter RAM name: "; cin >> ram;
+                break;
+
+            case 5:
+                cout << "Thank you!";
+                break;
+
+            default:
+                cout << "Invalid choice";
+                break;
+            }
+        }
     return 0;
 }
